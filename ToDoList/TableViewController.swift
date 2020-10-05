94ac84a0c6c759db68056c0d42c176f0941d1186
ikustow <ikustow@yandex.run>
2020-10-05 22:49:29 +0300

    add CoreData : insert and delete

--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
-    var tasks: [String] = []
+    
+    var tasks: [Task] = []
     
     @IBAction func saveTask(_ sender: UIBarButtonItem) {
         
         let alertController = UIAlertController(title: "New Task", message: "Please add new task", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { action in
             let tf = alertController.textFields?.first
-            if let newTask = tf?.text{
-                self.tasks.insert(newTask, at: 0)
+            if let newTaskTitle = tf?.text{
+                self.saveTask(withTitle: newTaskTitle)
                 self.tableView.reloadData()
             }
         }
@@ -29,14 +31,70 @@ class TableViewController: UITableViewController {
         
     }
     
+    private func saveTask(withTitle title: String){
+        
+        let context = getContext()
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context)
+        else {return}
+        
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func getContext() -> NSManagedObjectContext{
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        // Тестовый обратный порядок
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        //
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //Удаляем все при запуске
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        if let results = try? context.fetch(fetchRequest){
+            for object in results{
+                context.delete(object)
+                
+            }
+        }
+        do {
+            try context.save()
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        //
+        
     }
 
     // MARK: - Table view data source
@@ -55,7 +113,9 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        cell.textLabel?.text = tasks[indexPath.row]
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
 
         return cell
     }
